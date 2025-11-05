a85e60fe3ae2428c40e10d2767ae32a3612b7d1c
schectma <schectma@oregonstate.edu>
2025-11-05 10:54:18 -0500

    Added Vehicle Locations and DML.sql

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ app.use(methodOverride('_method'));
 
 app.use('/customers', require('./routes/customers'));
 app.use('/vehicles', require('./routes/vehicles'));
-// app.use('/locations', require('./routes/locations'));
+app.use('/locations', require('./routes/locations'));
 app.use('/rentals', require('./routes/rentals'));
+app.use('/vehicles_locations', require('./routes/vehicles_locations'));
 
 app.get('/', (req, res) => {
   res.redirect('/customers');
