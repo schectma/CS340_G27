29254d6f76bb3f69830428d7292dd0d79f0ce977
Alexander Schectman <schectma@classwork.engr.oregonstate.edu>
2025-11-06 09:52:52 -0800

    Production timeout fix

--- routes/rentals.js
+++ routes/rentals.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.get('/', (req, res) => {
-  db.query('SELECT * FROM Rentals', (err, results) => {
-    if (err) throw err;
+router.get('/', async (req, res) => {
+  try {
+    const [results] = await db.query('SELECT * FROM Rentals');
     res.render('rentals/index', { rentals: results });
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Database error');
+  }
 });
 
 module.exports = router;
--- routes/vehicles.js
+++ routes/vehicles.js
@@ -2,46 +2,61 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.get('/', (req, res) => {
-  db.query('SELECT * FROM Vehicles', (err, results) => {
-    if (err) throw err;
+router.get('/', async (req, res) => {
+  try {
+    const [results] = await db.query('SELECT * FROM Vehicles');
     res.render('vehicles/index', { vehicles: results });
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Database error');
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const { model, year, basePrice, isAvailable } = req.body;
   const available = isAvailable ? 1 : 0;
-  db.query('INSERT INTO Vehicles (model, year, basePrice, isAvailable) VALUES (?, ?, ?, ?)', [model, year, basePrice, available], (err) => {
-    if (err) throw err;
+  try {
+    await db.query('INSERT INTO Vehicles (model, year, basePrice, isAvailable) VALUES (?, ?, ?, ?)', [model, year, basePrice, available]);
     res.redirect('/vehicles');
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Database error');
+  }
 });
 
-router.get('/delete/:id', (req, res) => {
+router.get('/delete/:id', async (req, res) => {
   const { id } = req.params;
-  db.query('DELETE FROM Vehicles WHERE vehicleID = ?', [id], (err) => {
-    if (err) throw err;
+  try {
+    await db.query('DELETE FROM Vehicles WHERE vehicleID = ?', [id]);
     res.redirect('/vehicles');
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Database error');
+  }
 });
 
-router.get('/update/:id', (req, res) => {
+router.get('/update/:id', async (req, res) => {
   const { id } = req.params;
-  db.query('SELECT * FROM Vehicles WHERE vehicleID = ?', [id], (err, results) => {
-    if (err) throw err;
+  try {
+    const [results] = await db.query('SELECT * FROM Vehicles WHERE vehicleID = ?', [id]);
     res.render('vehicles/update', { vehicle: results[0] });
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Database error');
+  }
 });
 
-router.post('/update/:id', (req, res) => {
+router.post('/update/:id', async (req, res) => {
   const { id } = req.params;
   const { model, year, basePrice, isAvailable } = req.body;
   const available = isAvailable ? 1 : 0;
-  db.query('UPDATE Vehicles SET model = ?, year = ?, basePrice = ?, isAvailable = ? WHERE vehicleID = ?', [model, year, basePrice, available, id], (err) => {
-    if (err) throw err;
+  try {
+    await db.query('UPDATE Vehicles SET model = ?, year = ?, basePrice = ?, isAvailable = ? WHERE vehicleID = ?', [model, year, basePrice, available, id]);
     res.redirect('/vehicles');
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Database error');
+  }
 });
 
 module.exports = router;
