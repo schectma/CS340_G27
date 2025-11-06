b107078786bc85c3640d93d0bcd86d0ea975cb81
Alexander Schectman <156155021+schectma@users.noreply.github.com>
2025-11-05 21:38:51 -0500

    Uncomment locations route in app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(methodOverride('_method'));
 
 app.use('/customers', require('./routes/customers'));
 app.use('/vehicles', require('./routes/vehicles'));
-// app.use('/locations', require('./routes/locations'));
+app.use('/locations', require('./routes/locations'));
 app.use('/rentals', require('./routes/rentals'));
 
 app.get('/', (req, res) => {
